dce7474f8bbf980402bcad31d2ff9b1d00c2d563
weewey <chuayewwee@gmail.com>
2017-04-03 01:17:15 +0000

    created the blog index html page, connected with mongoose

--- app.js
+++ app.js
@@ -8,14 +8,41 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 //setting up mongoose, title, image, body, date
-mongoose.connect("mongodb//localhost/restfulblogapp");
+mongoose.connect("mongodb://localhost/restfulblogapp");
+
+//MONGOOSE/MODEL CONFIG
+var blogSchema =  new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+})
+
+var Blog = mongoose.model("Blog",blogSchema);
+
+
+// Blog.create({
+//     title:"The Start of Something New",
+//     image:"https://images.unsplash.com/reserve/unsplash_528b27288f41f_1.JPG",
+//     body:"Inspired by Casey who vlogs everyday. As a developer, I want to challenge myself and develop more skill. I'm starting a challenge to develop one apps every week, no matter how hideous they look"
+// });
+//RESTFUL ROUTES
 
 app.get("/", function(req,res){
-    res.send("You are connected");
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req,res){
+    Blog.find({}, function(err,blogs){
+        if(err){
+            console.log("ERROR :" + err);
+        } else{
+            res.render("index",{blogs:blogs});
+        }
+    })
 });
 
 app.listen(process.env.PORT,process.env.IP, function(){
     console.log("Your Restful Blog has started!")
 });
-
 
